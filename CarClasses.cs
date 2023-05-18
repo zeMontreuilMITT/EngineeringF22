e23dcf14f0aebb460af81c9f4aa2cd6bc15b081f
Zacharie Montreuil <zacharie.montreuil@mitt.ca>
2023-05-18 11:13:44 -0500

    Add project files.

---
+++ CarClasses.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+namespace EngineeringF22
+{
+    #region ConcreteClasses
+    public abstract class Car
+    {
+        protected IgnitionBehaviour _ignitionBehaviour;
+        protected RefuellingBehaviour _refuellingBehaviour;
+        public void StartCar()
+        {
+            _ignitionBehaviour.StartCar();
+        }
+
+        public void RefuelCar()
+        {
+            _refuellingBehaviour.RefuelCar();
+        }
+
+        public void Accelerate()
+        {
+            Console.WriteLine("The car goes faster");
+        }
+
+        public void Decelerate() 
+        {
+            Console.WriteLine("The car slows down.");
+        }
+
+        public abstract string Description();
+    }
+
+    public class ElectricCar: Car
+    {
+        public override string Description()
+        {
+            return "A car that runs solely on an electric battery.";
+        }
+    }
+    public class HyrbridCar: Car
+    {
+        public override string Description()
+        {
+            return "A car that runs on a gasoline engine that also charges an electric battery that can also run the engine.";
+        }
+
+        public void HybridCar()
+        {
+            _ignitionBehaviour = new EngineStartHybrid();
+            _refuellingBehaviour = new RefuelGasoline();
+        }
+    }
+    public class GasolineCar : Car
+    {
+        public override string Description() {
+            return "A car that uses gasoline as fuel for a combustion engine.";
+        }
+
+        public GasolineCar()
+        {
+            _ignitionBehaviour = new EngineStartGasoline();
+            _refuellingBehaviour = new RefuelGasoline();
+        }
+    }
+    public class DieselCar : Car
+    {
+        public override string Description()
+        {
+            return "A car that uses diesel as fuel for a combustion engine.";
+        }
+    }
+    public class HydrogenCar: Car
+    {
+        public override string Description()
+        {
+            return "A car that uses hydrogen as fuel for a hydrogen engine.";
+        }
+
+        public HydrogenCar()
+        {
+            _ignitionBehaviour = new EngineStartHydrogen();
+            _refuellingBehaviour = new RefuelHydrogen();
+        }
+    }
+    public class NuclearFusionCar: Car
+    {
+        public override string Description()
+        {
+            return "A car that uses hydrogen as fuel for a nuclear fusion engine.";
+        }
+
+        public NuclearFusionCar()
+        {
+            _refuellingBehaviour = new RefuelHydrogen();
+            _ignitionBehaviour = new EngineStartFusion();
+        }
+    }
+    #endregion
+
+
+    #region Behaviours
+
+    public interface IgnitionBehaviour
+    {
+        public void StartCar();
+    }
+    public class EngineStartGasoline: IgnitionBehaviour
+    {
+        public void StartCar()
+        {
+            Console.WriteLine("The car ignites its gasoline engine.");
+        }
+    }
+    public class EngineStartHybrid: IgnitionBehaviour
+    {
+        public void StartCar()
+        {
+            Console.WriteLine("The car ignites its gasoline engine and starts its electric motor.");
+        }
+    }
+    public class EngineStartHydrogen: IgnitionBehaviour
+    {
+        public void StartCar()
+        {
+            Console.WriteLine("The car starts its hydrogen engine.");
+        }
+    }
+    public class EngineStartFusion: IgnitionBehaviour
+    {
+        public void StartCar()
+        {
+            Console.WriteLine("The car somehow powers itself with nuclear fusion.");
+        }
+    }
+
+
+
+
+    public interface RefuellingBehaviour
+    {
+        public void RefuelCar();
+    }
+    public class RefuelGasoline: RefuellingBehaviour
+    {
+        public void RefuelCar()
+        {
+            Console.WriteLine("The car can't be refuelled because the world ran out of gasoline :( ");
+        }
+    }
+    public class RefuelHydrogen: RefuellingBehaviour
+    {
+        public void RefuelCar()
+        {
+            Console.WriteLine("The car's fuel tank is filled with hydrogen.");
+        }
+    }
+
+
+    #endregion
+}
---
+++ Program.cs
@@ -0,0 +1,146 @@
+﻿Duck mallard = new Mallard();
+
+Duck rubberDuck = new RubberDuck();
+
+mallard.PerformFlyBehaviour();
+mallard.PerformQuackBehaviour();
+// mallard duck gets tired
+mallard.FlyBehaviour = new FlyForTwoMinutes();
+mallard.PerformFlyBehaviour();
+
+rubberDuck.PerformFlyBehaviour();
+rubberDuck.PerformQuackBehaviour();
+
+public abstract class Duck
+{
+    protected QuackBehaviour QuackBehaviour { get; set; }
+    public FlyBehaviour FlyBehaviour { get; set; }
+
+    public void PerformFlyBehaviour()
+    {
+        FlyBehaviour.Fly();
+    }
+    public void PerformQuackBehaviour()
+    {
+        QuackBehaviour.Quack();
+    }
+
+
+    public void Swim()
+    {
+        Console.WriteLine("The duck swims around.");
+    }
+
+    public abstract void Display();
+}
+
+public class Mallard : Duck { 
+    public override void Display()
+    {
+        Console.WriteLine("This duck looks like a mallard.");
+    }
+
+    public Mallard()
+    {
+        FlyBehaviour = new FlyWithWings();
+        QuackBehaviour = new QuackLikeADuck();
+    }
+}
+
+// Redheads can only fly for two minutes
+public class RedHead: Duck
+{
+    public override void Display()
+    {
+        Console.WriteLine("This duck looks like a redhead.");
+    }
+
+    public RedHead()
+    {
+        FlyBehaviour = new FlyForTwoMinutes();
+        QuackBehaviour = new QuackLikeADuck();
+    }
+
+}
+
+public class RubberDuck: Duck
+{
+    public override void Display()
+    {
+        Console.WriteLine("Looks like a rubber duck.");
+    }
+
+    public RubberDuck()
+    {
+        FlyBehaviour = new FlyFlightless();
+        QuackBehaviour = new QuackSqueak();
+    }
+}
+
+public class WoodenDecoyDuck : Duck
+{
+    public override void Display()
+    {
+        Console.WriteLine("Looks like a real duck but made of wood.");
+    }
+
+    public WoodenDecoyDuck()
+    {
+        FlyBehaviour = new FlyFlightless();
+        QuackBehaviour = new QuackMute();
+    }
+}
+
+// FLY BEHAVIOUR
+public interface FlyBehaviour
+{
+    public void Fly();
+}
+public class FlyWithWings: FlyBehaviour
+{
+    public void Fly()
+    {
+        Console.WriteLine("The duck flaps its wings and flies around");
+    }
+}
+public class FlyFlightless: FlyBehaviour
+{
+    public void Fly()
+    {
+        Console.WriteLine("This duck cannot fly.");
+    }
+}
+public class FlyForTwoMinutes: FlyBehaviour
+{
+    public void Fly()
+    {
+        Console.WriteLine("The duck flies for two minutes at most.");
+    }
+}
+
+// QUACK BEHAVIOUR
+public interface QuackBehaviour
+{
+    public void Quack();
+}
+public class QuackLikeADuck : QuackBehaviour
+{
+    public void Quack()
+    {
+        Console.WriteLine("The duck makes the sound that most living ducks will make.");
+    }
+}
+public class QuackSqueak: QuackBehaviour
+{
+    public void Quack()
+    {
+        Console.WriteLine("Squeak squeak!");
+    }
+}
+public class QuackMute: QuackBehaviour
+{
+    public void Quack()
+    {
+        Console.WriteLine(". . .");
+    }
+}
